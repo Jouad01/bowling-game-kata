35b3d9c87bb6e0dcad7d155de7fa55c9a696889a
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-01-08 17:42:04 +0100

    feat: Codigo bowling

--- bowling.py
+++ bowling.py
@@ -5,3 +5,33 @@ class Bowling:
 
     def roll(self, pins):
         self.rolls.append(pins)
+    
+    def score(self):
+        resultado = 0
+        rollIndex = 0
+        for frameIndex in range(10):
+            if self.strike(rollIndex):
+                resultado += self.strikeScore(rollIndex)
+                rollIndex += 1
+            elif self.spare(rollIndex):
+                resultado += self.spareScore(rollIndex)
+                rollIndex += 2
+            else:
+                resultado += self.frameScore(rollIndex)
+                rollIndex += 2
+        return resultado
+    
+    def strike(self, rollIndex):
+        return self.rolls[rollIndex] == 10
+    
+    def spare(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
+    
+    def strikeScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+    
+    def spareScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 2]
+    
+    def frameScore(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
